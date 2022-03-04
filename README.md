c98ab832003f23180637e5e520b62169e6b32266
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2022-03-04 14:19:05 +1000

    initial commit: systat with base functionality
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

---
+++ README.md
@@ -0,0 +1,103 @@
+# sys_stat utility tool
+
+This cmd line tool provides easy access for system statistics to be displayed to user. Using the `sysinfo` crate, the app should be supported across all major OSs (Linux/Windows/Mac/OBSD). Currently tested only on Linux.
+
+Supported metrics:
+
+  * CPU Info
+  * CPU Stats (freq/usage)
+  * Ram/Swap Stats
+  * Network Stats (Up/Down)
+  * Hardware Temperature
+  * Disk Info
+
+Example usage linux:
+```SHELL
+$ ./sys_stat --help
+
+Simple program to monitor system statistics Can be used with `watch` for periodic statistics
+
+USAGE:
+    sys_stat [OPTIONS]
+
+OPTIONS:
+    -c, --cpu-info        cpu info
+    -d, --disk-info       disk stats
+    -f, --freq-cpu        cpu stats (freq/usage)
+    -h, --hw-temps        hardware temps
+        --help            Print help information
+    -n, --network-info
+    -o, --os-info         OS info
+    -r, --ram-info        ram/swap stats
+    -V, --version         Print version information
+```
+To see cpu info and current stats
+```SHELL
+$ ./sys_stat -f -c
+
+CPU Statistics:
+Core 0: 4701Hz, 2.50%
+Core 1: 4702Hz, 2.56%
+Core 2: 4701Hz, 7.32%
+Core 3: 4700Hz, 7.32%
+Core 4: 4702Hz, 2.50%
+Core 5: 4702Hz, 5.00%
+Core 6: 4701Hz, 5.00%
+Core 7: 4702Hz, 2.63%
+Core 8: 4700Hz, 7.50%
+Core 9: 4702Hz, 5.00%
+Core 10: 4700Hz, 5.00%
+Core 11: 4700Hz, 9.76%
+Avg Usage 6.1983%
+
+CPU info:
+Name - cpu0
+Brand - Intel(R) Core(TM) i5-10600K CPU @ 4.10GHz
+Vendor ID - GenuineIntel
+Cores - 12
+```
+This tool can be used with `watch` for **continuous monitoring.**
+```SHELL
+watch ./sys_stat -h
+
+Every 2.0s: ./sys_stat -h                                                          ArchDesktop: Fri Mar  4 14:34:48 2022
+
+Component - current temp, max temp, crit temp
+"Composite"       36.85°C,     83.85°C,     83.85°C
+"Composite"       46.85°C,     76.85°C,     79.85°C
+"Core 0"       51.0°C,     80.0°C,     100.0°C
+"Core 1"       49.0°C,     80.0°C,     100.0°C
+"Core 2"       51.0°C,     80.0°C,     100.0°C
+"Core 3"       51.0°C,     80.0°C,     100.0°C
+"Core 4"       51.0°C,     80.0°C,     100.0°C
+"Core 5"       51.0°C,     80.0°C,     100.0°C
+"edge"       58.0°C,     58.0°C,     100.0°C
+"junction"       60.0°C,     60.0°C,     110.0°C
+"mem"       62.0°C,     62.0°C,     100.0°C
+"Package id 0"       51.0°C,     80.0°C,     100.0°C
+"Sensor 1"       36.85°C,     65261.848°C,     0.0°C
+"Sensor 2"       54.85°C,     65261.848°C,     0.0°C
+"CPU"       27.8°C,     27.8°C,     0.0°C
+
+```
+or even
+```SHELL
+watch ./sys_stat -f
+
+Every 2.0s: ./sys_stat -f                                                          ArchDesktop: Fri Mar  4 14:35:19 2022
+
+CPU Statistics:
+Core 0: 4700Hz, 5.71%
+Core 1: 4700Hz, 8.33%
+Core 2: 4700Hz, 10.81%
+Core 3: 4701Hz, 8.33%
+Core 4: 4701Hz, 13.89%
+Core 5: 4700Hz, 10.81%
+Core 6: 4700Hz, 11.11%
+Core 7: 4700Hz, 8.11%
+Core 8: 4701Hz, 8.33%
+Core 9: 4700Hz, 16.22%
+Core 10: 4701Hz, 8.11%
+Core 11: 4702Hz, 5.71%
+Avg Usage 9.2166%
+```
---
+++ src/main.rs
@@ -0,0 +1,209 @@
+use clap::Parser;
+use colored::Colorize;
+use human_bytes::human_bytes;
+use std::{thread, time};
+use sysinfo::{ComponentExt, NetworkExt, NetworksExt, ProcessExt, ProcessorExt, System, SystemExt};
+
+/// Simple program to monitor system statistics
+/// Can be used with `watch` for periodic statistics
+#[derive(Parser, Debug)]
+#[clap(author = "Wilfred MK", version = "0.1", about, long_about = None)]
+struct Opts {
+    /// cpu info
+    #[clap(short, long, parse(from_occurrences))]
+    cpu_info: u8,
+
+    /// cpu stats (freq/usage)
+    #[clap(short, long, parse(from_occurrences))]
+    freq_cpu: u8,
+
+    /// hardware temps
+    #[clap(short, long, parse(from_occurrences))]
+    hw_temps: u8,
+
+    /// ram/swap stats
+    #[clap(short, long, parse(from_occurrences))]
+    ram_info: u8,
+
+    /// disk stats
+    #[clap(short, long, parse(from_occurrences))]
+    disk_info: u8,
+
+    /// OS info
+    #[clap(short, long, parse(from_occurrences))]
+    os_info: u8,
+
+    // network stats
+    #[clap(short, long, parse(from_occurrences))]
+    network_info: u8,
+}
+
+fn show_temps(sys: &sysinfo::System) {
+    println!(
+        "{}",
+        format!("Component - current temp, max temp, crit temp")
+            .purple()
+            .on_black()
+    );
+    for component in sys.components() {
+        println!(
+            "{:?}       {:?}°C,     {:?}°C,     {:?}°C",
+            component.label(),
+            component.temperature(),
+            component.max(),
+            match component.critical() {
+                Some(crit_temp) => crit_temp,
+                None => 0.0,
+            },
+        );
+    }
+}
+
+fn show_os_info(sys: &sysinfo::System) {
+    println!("OS info:");
+    println!("System name:             {}", sys.name().unwrap());
+    println!("System kernel version:   {}", sys.kernel_version().unwrap());
+    println!("System OS version:       {}", sys.os_version().unwrap());
+    println!("System host name:        {}", sys.host_name().unwrap());
+
+    let uptime = sys.uptime();
+    let seconds = uptime % 60;
+    let minutes = (uptime / 60) % 60;
+    let hours = (uptime / 60) / 60;
+    println!(
+        "System Uptime:           {}hrs:{}mns:{}s",
+        hours, minutes, seconds
+    );
+
+    let load_avg = sys.load_average();
+    println!(
+        "load_average:            one minute: {}%, five minutes: {}%, fifteen minutes: {}%",
+        load_avg.one, load_avg.five, load_avg.fifteen,
+    );
+}
+
+fn show_disk_info(sys: &sysinfo::System) {
+    println!("Disk info:");
+    println!("=> disks:");
+    for disk in sys.disks() {
+        println!("{:?}", disk);
+    }
+}
+
+fn show_ram_info(sys: &sysinfo::System) {
+    // RAM and swap information:
+    println!("Ram/Swap info:");
+    println!(
+        "total memory: {}",
+        human_bytes((sys.total_memory() * 1024) as f64)
+    );
+    println!(
+        "used memory : {}",
+        human_bytes((sys.used_memory() * 1024) as f64)
+    );
+    println!(
+        "free memory : {}",
+        human_bytes((sys.available_memory() * 1024) as f64)
+    );
+    println!(
+        "Ram Usage   : {:.2}%",
+        (sys.used_memory() as f64 / sys.total_memory() as f64) * 100.00
+    );
+    println!(
+        "total swap  : {}",
+        human_bytes((sys.total_swap() * 1024) as f64)
+    );
+    println!(
+        "used swap   : {}",
+        human_bytes((sys.used_swap() * 1024) as f64)
+    );
+}
+
+fn show_cpu_info(sys: &sysinfo::System) {
+    println!("CPU info:");
+    println!("Name - {}", sys.processors()[0].name());
+    println!("Brand - {}", sys.processors()[0].brand());
+    println!("Vendor ID - {}", sys.processors()[0].vendor_id());
+    println!("Cores - {}", sys.processors().len());
+}
+
+fn show_cpu_stats(sys: &sysinfo::System) {
+    println!("CPU Statistics:");
+    for i in 0..sys.processors().len() {
+        println!(
+            "Core {}: {}Hz, {:.2}%",
+            i,
+            sys.processors()[i].frequency(),
+            sys.processors()[i].cpu_usage()
+        );
+    }
+    println!("Avg Usage {:.4}%", sys.global_processor_info().cpu_usage());
+}
+
+fn show_network_stats(sys: &sysinfo::System) {
+    println!("Network Stats:");
+    println!("=> networks:");
+    for (interface_name, data) in sys.networks() {
+        println!(
+            "{}: {}/{} B",
+            interface_name,
+            data.received(),
+            data.transmitted()
+        );
+    }
+}
+
+fn run_stats(sys: &mut sysinfo::System, opts: &Opts) {
+    match opts.os_info {
+        0 => {}
+        _ => {
+            show_os_info(sys);
+        }
+    }
+    match opts.hw_temps {
+        0 => {}
+        _ => {
+            show_temps(sys);
+        }
+    }
+    match opts.freq_cpu {
+        0 => {}
+        _ => {
+            show_cpu_stats(sys);
+        }
+    }
+    match opts.disk_info {
+        0 => {}
+        _ => {
+            show_disk_info(sys);
+        }
+    }
+
+    match opts.cpu_info {
+        0 => {}
+        _ => {
+            show_cpu_info(sys);
+        }
+    }
+
+    match opts.ram_info {
+        0 => {}
+        _ => {
+            show_ram_info(sys);
+        }
+    }
+
+    match opts.network_info {
+        0 => {}
+        _ => {
+            show_network_stats(sys);
+        }
+    }
+}
+
+fn main() {
+    let mut sys = System::new_all();
+    let opts = Opts::parse();
+    sys.refresh_all();
+    run_stats(&mut sys, &opts);
+}
