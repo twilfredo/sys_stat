e044b2a24105460e4c66b7f3bf84b86df3d6c2fc
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2022-09-17 13:31:16 +1000

    systat: bump crates and split libs
    Split out the system dislpay functions into
    it's own file with a struct to hold `sys` and
    implemented functions to do ops.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
+pub mod sensors;
+use crate::sensors::system::*;
 use clap::Parser;
-use colored::Colorize;
 use human_bytes::human_bytes;
-use std::{thread, time};
-use sysinfo::{ComponentExt, NetworkExt, NetworksExt, ProcessExt, ProcessorExt, System, SystemExt};
 
 /// Simple program to monitor system statistics
 /// Can be used with `watch` for periodic statistics
@@ -38,172 +37,58 @@ struct Opts {
     network_info: u8,
 }
 
-fn show_temps(sys: &sysinfo::System) {
-    println!(
-        "{}",
-        format!("Component\t\tcurrent temp,\t\tmax temp,\t\tcrit temp")
-            .purple()
-            .on_black()
-    );
-    for component in sys.components() {
-        println!(
-            "{0: <10}\t\t{1: <10}°C,\t\t{2: <10}°C,\t\t{3: <10}°C",
-            component.label(),
-            component.temperature(),
-            component.max(),
-            match component.critical() {
-                Some(crit_temp) => crit_temp,
-                None => 0.0,
-            },
-        );
-    }
-}
-
-fn show_os_info(sys: &sysinfo::System) {
-    println!("OS info:");
-    println!("System name:             {}", sys.name().unwrap());
-    println!("System kernel version:   {}", sys.kernel_version().unwrap());
-    println!("System OS version:       {}", sys.os_version().unwrap());
-    println!("System host name:        {}", sys.host_name().unwrap());
-
-    let uptime = sys.uptime();
-    let seconds = uptime % 60;
-    let minutes = (uptime / 60) % 60;
-    let hours = (uptime / 60) / 60;
-    println!(
-        "System Uptime:           {}hrs:{}mns:{}s",
-        hours, minutes, seconds
-    );
-
-    let load_avg = sys.load_average();
-    println!(
-        "load_average:            one minute: {}%, five minutes: {}%, fifteen minutes: {}%",
-        load_avg.one, load_avg.five, load_avg.fifteen,
-    );
-}
-
-fn show_disk_info(sys: &sysinfo::System) {
-    println!("Disk info:");
-    println!("=> disks:");
-    for disk in sys.disks() {
-        println!("{:?}", disk);
-    }
-}
-
-fn show_ram_info(sys: &sysinfo::System) {
-    // RAM and swap information:
-    println!("Ram/Swap info:");
-    println!(
-        "total memory: {}",
-        human_bytes((sys.total_memory() * 1024) as f64)
-    );
-    println!(
-        "used memory : {}",
-        human_bytes((sys.used_memory() * 1024) as f64)
-    );
-    println!(
-        "free memory : {}",
-        human_bytes((sys.available_memory() * 1024) as f64)
-    );
-    println!(
-        "Ram Usage   : {:.2}%",
-        (sys.used_memory() as f64 / sys.total_memory() as f64) * 100.00
-    );
-    println!(
-        "total swap  : {}",
-        human_bytes((sys.total_swap() * 1024) as f64)
-    );
-    println!(
-        "used swap   : {}",
-        human_bytes((sys.used_swap() * 1024) as f64)
-    );
-}
-
-fn show_cpu_info(sys: &sysinfo::System) {
-    println!("CPU info:");
-    println!("Name - {}", sys.processors()[0].name());
-    println!("Brand - {}", sys.processors()[0].brand());
-    println!("Vendor ID - {}", sys.processors()[0].vendor_id());
-    println!("Cores - {}", sys.processors().len());
-}
-
-fn show_cpu_stats(sys: &sysinfo::System) {
-    println!("CPU Statistics:");
-    for i in 0..sys.processors().len() {
-        println!(
-            "Core {}: {}Hz, {:.2}%",
-            i,
-            sys.processors()[i].frequency(),
-            sys.processors()[i].cpu_usage()
-        );
-    }
-    println!("Avg Usage {:.4}%", sys.global_processor_info().cpu_usage());
-}
-
-fn show_network_stats(sys: &sysinfo::System) {
-    println!("Network Stats:");
-    println!("=> networks:");
-    for (interface_name, data) in sys.networks() {
-        println!(
-            "{}: {}/{} B",
-            interface_name,
-            data.received(),
-            data.transmitted()
-        );
-    }
-}
-
-fn run_stats(sys: &mut sysinfo::System, opts: &Opts) {
+fn run_stats(sys: &Sensors, opts: &Opts) {
     match opts.os_info {
         0 => {}
         _ => {
-            show_os_info(sys);
+            sys.show_os_info();
         }
     }
     match opts.hw_temps {
         0 => {}
         _ => {
-            show_temps(sys);
+            sys.show_temps();
         }
     }
     match opts.freq_cpu {
         0 => {}
         _ => {
-            show_cpu_stats(sys);
+            sys.show_cpu_stats();
         }
     }
     match opts.disk_info {
         0 => {}
         _ => {
-            show_disk_info(sys);
+            sys.show_disk_info();
         }
     }
 
     match opts.cpu_info {
         0 => {}
         _ => {
-            show_cpu_info(sys);
+            sys.show_cpu_info();
         }
     }
 
     match opts.ram_info {
         0 => {}
         _ => {
-            show_ram_info(sys);
+            sys.show_ram_info();
         }
     }
 
     match opts.network_info {
         0 => {}
         _ => {
-            show_network_stats(sys);
+            sys.show_network_stats();
         }
     }
 }
 
 fn main() {
-    let mut sys = System::new_all();
+    let mut sys = sensors::system::Sensors::new();
+
     let opts = Opts::parse();
-    sys.refresh_all();
-    run_stats(&mut sys, &opts);
+    sys.refresh_sens();
+    run_stats(&sys, &opts);
 }
---
+++ src/sensors/system.rs
@@ -0,0 +1,154 @@
+use crate::human_bytes;
+use colored::Colorize;
+use sysinfo::{ComponentExt, CpuExt, NetworkExt, System, SystemExt};
+
+pub struct Sensors {
+    sys: sysinfo::System,
+}
+
+impl Sensors {
+    pub fn new() -> Self {
+        Sensors {
+            sys: System::new_all(),
+        }
+    }
+
+    pub fn refresh_sens(&mut self) {
+        self.sys.refresh_all();
+    }
+
+    pub fn show_temps(&self) {
+        println!(
+            "{}",
+            format!("Component\t\tcurrent temp,\t\tmax temp,\t\tcrit temp")
+                .purple()
+                .on_black()
+        );
+        for component in self.sys.components() {
+            println!(
+                "{0: <10}\t\t{1: <10}°C,\t\t{2: <10}°C,\t\t{3: <10}°C",
+                component.label(),
+                component.temperature(),
+                component.max(),
+                match component.critical() {
+                    Some(crit_temp) => crit_temp,
+                    None => 0.0,
+                },
+            );
+        }
+    }
+
+    pub fn show_os_info(&self) {
+        println!("OS info:");
+        println!(
+            "System name:             {}",
+            self.sys.name().unwrap_or("N/A".to_string())
+        );
+        println!(
+            "System kernel version:   {}",
+            self.sys.kernel_version().unwrap_or("N/A".to_string())
+        );
+        println!(
+            "System OS version:       {}",
+            self.sys.os_version().unwrap_or("N/A".to_string())
+        );
+        println!(
+            "System host name:        {}",
+            self.sys.host_name().unwrap_or("N/A".to_string())
+        );
+
+        let uptime = self.sys.uptime();
+        let seconds = uptime % 60;
+        let minutes = (uptime / 60) % 60;
+        let hours = (uptime / 60) / 60;
+        println!(
+            "System Uptime:           {}hrs:{}mns:{}s",
+            hours, minutes, seconds
+        );
+
+        let load_avg = self.sys.load_average();
+        println!(
+            "load_average:            one minute: {}%, five minutes: {}%, fifteen minutes: {}%",
+            load_avg.one, load_avg.five, load_avg.fifteen,
+        );
+    }
+
+    pub fn show_disk_info(&self) {
+        println!("Disk info:");
+        println!("=> disks:");
+        for disk in self.sys.disks() {
+            println!("{:?}", disk);
+        }
+    }
+
+    pub fn show_ram_info(&self) {
+        // RAM and swap information:
+        println!("Ram/Swap info:");
+        println!(
+            "total memory: {}",
+            human_bytes((self.sys.total_memory() * 1024) as f64)
+        );
+        println!(
+            "used memory : {}",
+            human_bytes((self.sys.used_memory() * 1024) as f64)
+        );
+        println!(
+            "free memory : {}",
+            human_bytes((self.sys.available_memory() * 1024) as f64)
+        );
+        println!(
+            "Ram Usage   : {:.2}%",
+            (self.sys.used_memory() as f64 / self.sys.total_memory() as f64) * 100.00
+        );
+        println!(
+            "total swap  : {}",
+            human_bytes((self.sys.total_swap() * 1024) as f64)
+        );
+        println!(
+            "used swap   : {}",
+            human_bytes((self.sys.used_swap() * 1024) as f64)
+        );
+    }
+
+    pub fn show_cpu_info(&self) {
+        println!("CPU info:");
+        let cpu = self.sys.global_cpu_info();
+        println!("Name - {}", cpu.name());
+        println!("Brand - {}", cpu.brand());
+        println!("Vendor ID - {}", cpu.vendor_id());
+        println!("Cores - {}", self.sys.physical_core_count().unwrap_or(0));
+    }
+
+    pub fn show_cpu_stats(&self) {
+        println!("CPU Statistics:");
+        let mut core_count = 0;
+        for proc in self.sys.cpus() {
+            println!(
+                "Core {}: {}Hz, {:.2}%",
+                core_count,
+                proc.frequency(),
+                proc.cpu_usage(),
+            );
+            core_count += 1;
+        }
+        println!(
+            "Load Average: one minute: {}%, five minutes: {}%, fifteen minutes: {}%",
+            self.sys.load_average().one,
+            self.sys.load_average().five,
+            self.sys.load_average().fifteen,
+        );
+    }
+
+    pub fn show_network_stats(&self) {
+        println!("Network Stats:");
+        println!("=> networks:");
+        for (interface_name, data) in self.sys.networks() {
+            println!(
+                "{}: {}/{} B",
+                interface_name,
+                data.received(),
+                data.transmitted()
+            );
+        }
+    }
+}
